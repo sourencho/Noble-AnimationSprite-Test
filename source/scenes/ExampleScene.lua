243a4fdee5a665675c23ea0b3cf595fb69e25f58
sourencho <souren@papazian.info>
2024-03-04 12:05:49 -0500

    Fix formatting

--- source/scenes/ExampleScene.lua
+++ source/scenes/ExampleScene.lua
@@ -3,23 +3,23 @@ class("ExampleScene").extends(NobleScene)
 local scene = ExampleScene
 
 function scene:init()
-	scene.super.init(self)
+    scene.super.init(self)
 
 end
 
 function scene:enter()
-	scene.super.enter(self)
+    scene.super.enter(self)
 
 
     scene.cloudSprite = NobleSprite(
-		"assets/images/cloud_1.png"
-	)
-	scene.cloudSprite:add()
-	scene.cloudSprite:setZIndex(10)
-	scene.cloudSprite:moveTo(200, 100)
+        "assets/images/cloud_1.png"
+    )
+    scene.cloudSprite:add()
+    scene.cloudSprite:setZIndex(10)
+    scene.cloudSprite:moveTo(200, 100)
 
 
-	scene.tileImage = Graphics.imagetable.new("assets/images/bird")
+    scene.tileImage = Graphics.imagetable.new("assets/images/bird")
     scene.tileAnimation = Noble.Animation.new(scene.tileImage)
     scene.tileAnimation:addState(
         "idle",
@@ -27,15 +27,15 @@ function scene:enter()
         scene.tileAnimation.imageTable:getLength()
     )
     scene.tileSprite = NobleSprite(scene.tileAnimation)
-	scene.tileSprite:add()
+    scene.tileSprite:add()
     scene.tileSprite:setZIndex(9)
-	scene.tileSprite:moveTo(200, 100)
+    scene.tileSprite:moveTo(200, 100)
 end
 
 function scene:update()
-	scene.super.update(self)
+    scene.super.update(self)
 
-	-- need this draw call to see the sprite
-	-- but it does not respect ZIndex
-	-- scene.tileAnimation:draw(200, 100)
+    -- need this draw call to see the sprite
+    -- but it does not respect ZIndex
+    -- scene.tileAnimation:draw(200, 100)
 end
